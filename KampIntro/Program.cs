33ba286d1fc92082eb2d28ec33fab11de3d1d8c0
caganert <caganegeertugrul@gmail.com>
2022-01-16 17:05:45 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace KampIntro
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //type safety - tip güvenliği
+            //Do not repeat yourself - kendini tekrarlama
+
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+            double dolarDun = 18.50;
+            double dolarBugun = 13.50;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azaış Oku");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Oku");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi");
+            }
+
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Loops 
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya başlangıç için temel kurs";
+            string kurs3 = "Java";
+
+            //array - dizi
+
+            string[] kurslar = new string[]
+            {
+              "Yazılım Geliştirici Yetiştirme Kampı", "Programlamaya başlangıç için temel kurs", "Java", "Python"
+            };
+
+            for (int i = 0; i<kurslar.Length; i++) //i++ (1 arttırır)   i=i+n veya i+=n (n kadar arttırır)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("-----------");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa Sonu - footer");
+        }
+    }
+}
